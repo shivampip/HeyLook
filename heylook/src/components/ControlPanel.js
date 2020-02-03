ab1020939e8d385ebfedceebaa270faca79b31cb
Shivam Agrawal <shivam301296@gmail.com>
2020-02-04 00:32:45 +0530

    updated

--- heylook/src/components/ControlPanel.js
+++ heylook/src/components/ControlPanel.js
@@ -2,10 +2,32 @@ import "./ControlPanel.css";
 import React from "react";
 
 class ControlPanel extends React.Component {
+	state = {
+		cameraClass: "btnActive",
+		uploadClass: "btnInActive"
+	};
+	setCamera = () => {
+		this.setState({ cameraClass: "btnActive", uploadClass: "btnInactive" });
+	};
+
+	setUpload = () => {
+		this.setState({ cameraClass: "btnInactive", uploadClass: "btnActive" });
+	};
+
 	render() {
 		return (
 			<div className="control-panel">
-				<div>CP</div>
+				<div className="wrapper">
+					<div className="screen"></div>
+					<div className="uploadBtnWrapper">
+						<button onClick={this.setCamera} className={this.state.cameraClass}>
+							Camera
+						</button>
+						<button onClick={this.setUpload} className={this.state.uploadClass}>
+							Upload
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
