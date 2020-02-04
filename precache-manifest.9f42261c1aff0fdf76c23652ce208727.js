48e9f768c0ad128d603c62008147053d8e5d7aa7
Shivam Agrawal <shivam301296@gmail.com>
2020-02-04 09:51:01 +0530

    Updates

---
+++ precache-manifest.9f42261c1aff0fdf76c23652ce208727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acee1c43cdb87017d3c6af51df4c89e9",
+    "url": "/HeyLook/index.html"
+  },
+  {
+    "revision": "da1ca9be2ad777e94643",
+    "url": "/HeyLook/static/css/main.123ed190.chunk.css"
+  },
+  {
+    "revision": "865403117be775bbc84b",
+    "url": "/HeyLook/static/js/2.8384d5b8.chunk.js"
+  },
+  {
+    "revision": "d0486a8127004fa41544ac8b03be8f42",
+    "url": "/HeyLook/static/js/2.8384d5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1ca9be2ad777e94643",
+    "url": "/HeyLook/static/js/main.af3e9a6b.chunk.js"
+  },
+  {
+    "revision": "72420e459ce792c1b60b",
+    "url": "/HeyLook/static/js/runtime-main.af16c78e.js"
+  },
+  {
+    "revision": "08f0206cae56740298d9b4e719af543d",
+    "url": "/HeyLook/static/media/shivam.08f0206c.jpg"
+  }
+]);
