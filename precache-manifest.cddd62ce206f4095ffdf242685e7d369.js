0d10f5dc46492ec8c08bd5f867175ab703643255
Shivam Agrawal <shivam301296@gmail.com>
2020-02-05 20:32:44 +0530

    Updates

--- precache-manifest.cddd62ce206f4095ffdf242685e7d369.js
+++ precache-manifest.cddd62ce206f4095ffdf242685e7d369.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5daa98fb31c9417b78380d40c055c25e",
+    "revision": "368a383f722905bd776177f7e6af0b48",
     "url": "/HeyLook/index.html"
   },
   {
-    "revision": "3c514b2e973e011d63a5",
+    "revision": "45cc80634e5b761c38b6",
     "url": "/HeyLook/static/css/main.123ed190.chunk.css"
   },
   {
-    "revision": "5b02963b8e88f3e20420",
-    "url": "/HeyLook/static/js/2.61af46bc.chunk.js"
+    "revision": "e362c95264501aab96c4",
+    "url": "/HeyLook/static/js/2.bfb997d1.chunk.js"
   },
   {
     "revision": "d0486a8127004fa41544ac8b03be8f42",
-    "url": "/HeyLook/static/js/2.61af46bc.chunk.js.LICENSE.txt"
+    "url": "/HeyLook/static/js/2.bfb997d1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3c514b2e973e011d63a5",
-    "url": "/HeyLook/static/js/main.c085b0dc.chunk.js"
+    "revision": "45cc80634e5b761c38b6",
+    "url": "/HeyLook/static/js/main.2c4c4282.chunk.js"
   },
   {
     "revision": "72420e459ce792c1b60b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HeyLook/precache-manifest.adae729d600efc30405ca43854e99456.js"
+  "/HeyLook/precache-manifest.cddd62ce206f4095ffdf242685e7d369.js"
 );
 
 self.addEventListener('message', (event) => {
